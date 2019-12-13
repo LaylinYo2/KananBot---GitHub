99a179de40aae49339d486f2c471691da271a326
LaylinYo2 <56234214+LaylinYo2@users.noreply.github.com>
2019-12-13 12:41:28 +0200

    Add files via upload

---
+++ commands/zahando.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You are not worthy.");
+    if(!args[0]) return message.channel.send("Specify an amount. | **Usage:** `>zahando <amount>`");
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`Scraped away **${args[0]}** messages.`).then(msg => msg.delete(5000));
+
+        let zaHando = new Discord.RichEmbed()
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`**Bulk Delete issued in ${message.channel}**\n**${args[0]} messages deleted.**`)
+        .setTimestamp()
+        .setColor("#e74c3c")
+    
+        let sChannel = bot.channels.find(r => r.name === 'kananlogs');
+        sChannel.send(zaHando);
+    })
+}
+
+module.exports.help = {
+    name: "zahando"
+}
